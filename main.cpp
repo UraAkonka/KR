8e6dd1be630e8547be54f4b3847e61996d768bb2
UraAkonka <lebzyak321@mail.ru>
2024-12-18 13:20:36 +0500

    Добавление структуры и также include vector windows.h fstream

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-
+#include <windows.h>
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+#pragma pack(push,1)
+
+struct pngfile{
+    uint32_t lenght;
+    uint32_t type;
+};
+
+#pragma pack(pop)
 
 int main(){
     return 0;
